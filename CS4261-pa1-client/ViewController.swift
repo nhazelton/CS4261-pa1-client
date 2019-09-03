d69906bf22b371186c4e06e22d822d81c44348e4
Nate Hazelton <natehazelnut@gmail.com>
2019-09-03 16:13:32 -0400

    Initial Commit

---
+++ CS4261-pa1-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS4261-pa1-client
+//
+//  Created by Nate Hazelton on 9/3/19.
+//  Copyright © 2019 Nate Hazelton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
