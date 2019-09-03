89c03670f11fb07d726ae2079eaa817756d8d8c5
Nate Hazelton <natehazelnut@gmail.com>
2019-09-03 18:08:25 -0400

    Implemented sample HTTP request w/json unpacking

--- CS4261-pa1-client/ViewController.swift
+++ CS4261-pa1-client/ViewController.swift
@@ -11,18 +11,30 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var dataLabel: UILabel!
     
+    struct Response: Codable {
+        let userId: Int
+        let id: Int
+        let title: String
+        let completed: Bool
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
     @IBAction func getDataButtonAction(_ sender: UIButton) {
-        let url = URL(string: "https://postman-echo.com/get?text=hey")!
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
         
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-            guard let data = data else { return }
-            print(String(data: data, encoding: .utf8)!)
+            if let data = data {
+                do {
+                    let json = try JSONDecoder().decode(Response.self, from: data)
+                    print(json.title)
+                } catch {
+                    print ("Something went wrong")
+                }
+            }
         }
         
         task.resume()
